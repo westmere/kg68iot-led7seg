b25d180da8c437a7171bbc006ef4c908dca3ca13
vinh <vinh@debian9>
2018-02-24 10:25:16 +0700

    first commit

---
+++ led7seg.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "led7seg.h"
+
+#ifndef assert
+#define assert(c)
+#endif
+#ifndef Log_info
+#define Log_info(fmt,args...)
+#endif
+#ifndef Log_error
+#define Log_error(fmt,args...)
+#endif
---
+++ led7seg.h
@@ -0,0 +1,14 @@
+#ifndef __KG68IOT_LED7SEG_H_
+#define __KG68IOT_LED7SEG_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __KG68IOT_LED7SEG_H_ */
