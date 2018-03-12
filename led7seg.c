6ed8ebd0b1f3f16776da3ef9e6d25fa661caaa8b
vinh <vinh@debian9>
2018-03-12 10:19:10 +0700

    ...

--- led7seg.c
+++ led7seg.c
@@ -3,12 +3,29 @@
 #include <string.h>
 #include "led7seg.h"
 
-#ifndef assert
-#define assert(c)
-#endif
-#ifndef Log_info
-#define Log_info(fmt,args...)
-#endif
-#ifndef Log_error
-#define Log_error(fmt,args...)
-#endif
+
+//	115200 8N1
+//	aa 55 02 cc 03 d1
+//	aa 55 02 cc 03 d1
+//	aa 55 02 cc 03 d1
+//	aa 55 02 cc 03 d1
+//	<0xaa> <0x55> <length:1> <icon:1> <number:1> <checksum:1>
+
+int kg68iot_led7_pdu(uint8_t *buf, int length, uint8_t digit, uint8_t icon)
+{
+	int i;
+
+	if(length < 6)
+		return -1;
+	buf[0] = 0xaa;
+	buf[1] = 0x55;
+	buf[2] = 2;
+	buf[3] = digit;
+	buf[4] = icon;
+	buf[5] = buf[2] + buf[3] + buf[4];
+	for(i=6;i<length || i < 10;i++)
+	{
+		buf[i] = 0xff;
+	}
+	return i;
+}
--- led7seg.h
+++ led7seg.h
@@ -7,6 +7,16 @@
 extern "C" {
 #endif
 
+#define KG68IOT_DIGIT_E0	(0xcc)
+#define KG68IOT_DIGIT_E1	(0xcb)
+#define KG68IOT_DIGIT_E2	(0xcb)
+
+#define KG68IOT_ICON_WIFI_FAST_BLINK	(0x01)
+#define KG68IOT_ICON_WIFI_SOLID			(0x02)
+#define KG68IOT_ICON_WIFI_BLINK			(0x03)
+
+int kg68iot_led7_pdu(uint8_t *buf, int length, uint8_t digit, uint8_t icon);
+
 #ifdef __cplusplus
 }
 #endif
